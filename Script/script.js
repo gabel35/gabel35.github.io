27b21feda97d4e2bcc9a9c3d276d3fcbe601ade3
gabel35 <gabrieljose3135@gmail.com>
2021-01-12 18:58:47 -0500

    fixed some links

--- Script/script.js
+++ Script/script.js
@@ -4,44 +4,44 @@ var carouselDiv = $(".carousel-inner");
 for (let i = 0; i<bgImgs.length; i++){
     var carouselImgs = $(`
     <div id="carousel-items" class="carousel-item">
-        <img class="d-block w-100" src="../Assets/Images/background/bella${bgImgs[i]}.JPG" alt="Pictures of my dog, Bella" style="">
+        <img class="d-block w-100" src="./Assets/Images/background/bella${bgImgs[i]}.JPG" alt="Pictures of my dog, Bella" style="">
     </div>`);
     carouselDiv.append(carouselImgs);
 }
 
-// What Weather Button Links //
-$("#whatWeather").on("click", function () {
-    window.location.href = "https://gabel35.github.io/What_Weather";
-})
+// // What Weather Button Links //
+// $("#whatWeather").on("click", function () {
+//     window.location.href = "https://gabel35.github.io/What_Weather";
+// })
 
-$("#wwRepo").on("click", function () {
-    window.location.href = "https://github.com/gabel35/What_Weather";
-})
+// $("#wwRepo").on("click", function () {
+//     window.location.href = "https://github.com/gabel35/What_Weather";
+// })
 
-//9-to-5 Planner Button Links //
-$("#hourlyPlanner").on("click", function () {
-    window.location.href = "https://gabel35.github.io/9-to-5_Planner/";
-})
+// //9-to-5 Planner Button Links //
+// $("#hourlyPlanner").on("click", function () {
+//     window.location.href = "https://gabel35.github.io/9-to-5_Planner/";
+// })
 
-$("#hpRepo").on("click", function () {
-    window.location.href = "https://github.com/gabel35/9-to-5_Planner";
-})
+// $("#hpRepo").on("click", function () {
+//     window.location.href = "https://github.com/gabel35/9-to-5_Planner";
+// })
 
-// Spyglass Button Links //
-$("#spyGlass").on("click", function () {
-    window.location.href = "https://gabel35.github.io/Spyglass/";
-})
+// // Spyglass Button Links //
+// $("#spyGlass").on("click", function () {
+//     window.location.href = "https://gabel35.github.io/Spyglass/";
+// })
 
-$("#sgRepo").on("click", function () {
-    window.location.href = "https://github.com/gabel35/Spyglass";
-})
+// $("#sgRepo").on("click", function () {
+//     window.location.href = "https://github.com/gabel35/Spyglass";
+// })
 
-// LinkedIn Link //
-$("#linkedIn").on("click", function () {
-    window.open("https://www.linkedin.com/in/gabriel-sanchez-919b88a1");
-})
+// // LinkedIn Link //
+// $("#linkedIn").on("click", function () {
+//     window.open("https://www.linkedin.com/in/gabriel-sanchez-919b88a1");
+// })
 
-// Github Link //
-$("#gitHub").on("click", function () {
-    window.open("https://github.com/gabel35");
-})
+// // Github Link //
+// $("#gitHub").on("click", function () {
+//     window.open("https://github.com/gabel35");
+// })
